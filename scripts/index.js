6a5cac778e8b61f21e2558082fb434bad624b994
Bruno Tetzner <brunoltetzner@gmail.com>
2021-10-05 17:19:05 -0300

    funçao diagonal direita finalizada

--- scripts/index.js
+++ scripts/index.js
@@ -1,13 +1,13 @@
 
 let tabuleiro =
  [
+    '     2',
+    '    2 ',
+    '   2  ',
+    '  2   ',
     '      ',
     '      ',
-    '      ',
-    '   1  ',
-    '  1   ',
-    ' 1    ',
-    '1     ',]
+    '      ',]
 
 
 const JOGADORES = {"jogador1": "", "jogador2": ""}
@@ -56,7 +56,6 @@ document.querySelector(".startSection button").addEventListener("click", () => {
         start()
     }
 })
-
 
 function start(){
     const sectionStart = document.querySelector(".startSection")
@@ -109,45 +108,37 @@ function vitoriaVertical(tabuleiro){
 }
 //
 // ONDE '1' REPRESENTA O JOGADOR 1, E '2', O JOGADOR 2
-let soma1 = 0; 
-let soma2 = 0 
 
 function victoryDD(){
-  
- for(let i = 5; i>=tabuleiro.length;i--){
-      for(let j = 0; j< tabuleiro[i].length;j++){
+  let soma1 = 0; 
+let soma2 = 0 
+
+ for(let i = 0; i<3;i++){
+      for(let j = 4; j>=0;j--){ 
        if(tabuleiro[i][j] === '1'){
-         console.log('v1')
-         soma1+=1
-         if(tabuleiro[i]-1[j]+1 === '1'){
-          console.log('v2')
+        for(let k = 1; k<4;k++){
+          if(tabuleiro[i][j] === tabuleiro[i+k][j-k]){
             soma1+=1
-            if(tabuleiro[i]+2[j]+2 === '1'){
-              console.log('v3')
-             soma1+=1
-             if(tabuleiro[i]+3 [j]+3 === '1')
-                console.log('jogador 1 venceu')
-                else{soma1 = 0;}
-              }
-              
-            }
-          }
-        
-        if(tabuleiro[i][j] === '2'){
-           soma2+=1
-          if(tabuleiro[i]-1[j]-1 === '2'){
-            soma2+=1
-            if(tabuleiro[i]-2[j]-2=== '2'){
-             soma2+=1
-             if(tabuleiro[i]-3 [j]-3 === '2')
-                console.log('jogador 2 venceu')
-              }
-              else{soma2=0}
-            }
-          }
+            console.log(soma1)
+          } 
         }
-        
-      }      
+       }
+        if(tabuleiro[i][j] === '2'){
+           for(let k = 1; k<4;k++){
+             if(tabuleiro[i][j] === tabuleiro[i+k][j-k]){
+                soma2+=1
+                console.log(soma2)
+              }
+            } 
+         }
+    }      
+  }
+  if(soma1 === 3){
+    console.log('O jogador 1 ganhou') 
+  }
+ else if(soma2 === 3) {console.log('O jogador 2 ganhou')}
+
 }
+
 victoryDD()
 
