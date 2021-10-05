c93ef06273f9b2ef8d56fd35c168904e570952b9
Bernardo <bernardocardosodacosta@gmail.com>
2021-10-04 21:05:05 -0300

    A adicionar evento de click nas divs.

--- scripts/index.js
+++ scripts/index.js
@@ -7,13 +7,36 @@ let tabuleiro= ['      ',
 '      ',]
 
 const Todotabuleiro = document.getElementById('tabuleiro-divs')
+
 for(let i =0; i<tabuleiro.length;i++){
-const tabuleiroLinha = document.createElement('section')
-tabuleiroLinha.classList.add('tabuleiro-divs__column')
-for(let j =0; j<6;j++){
-const celulaDiv = document.createElement('div')
-celulaDiv.classList.add('tabuleiro-divs__divs')
-tabuleiroLinha.appendChild(celulaDiv)
+    const tabuleiroLinha = document.createElement('section')
+    tabuleiroLinha.classList.add('tabuleiro-divs__column')
+    for(let j =0; j<6;j++){
+        const celulaDiv = document.createElement('div')
+        celulaDiv.classList.add('tabuleiro-divs__divs')
+        tabuleiroLinha.appendChild(celulaDiv)
+    }
+    Todotabuleiro.appendChild(tabuleiroLinha)
 }
-Todotabuleiro.appendChild(tabuleiroLinha)
+/*
+let atual = "vermelho"
+let proximo = "preto"
+
+let discosVermelhos = []
+let discosPretos = []
+let discosTotais = []
+*/
+const cells = document.querySelectorAll(".tabuleiro-divs__divs")
+console.log(cells)
+
+cells.addEventListener("click", event => {
+    console.log(event.target.value)
+})
+// não estou conseguindo selecionar as células para adicionar o evento de click nelas.
+
+/*
+const cells = document.querySelector(".tabuleiro-divs__divs")
+for(let i = 0; i < cells.length; i++) {
+    cells[i].addEventListener("click", handleClick)
 }
+*/
