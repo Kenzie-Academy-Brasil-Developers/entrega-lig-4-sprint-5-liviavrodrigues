f00b49d9eabba36acf3df3f38a3432178008a01c
PauloJose26 <paulojoliveira.2616@gmail.com>
2021-10-04 21:18:13 -0300

    Funçao Start finalizada

--- scripts/index.js
+++ scripts/index.js
@@ -1,19 +1,75 @@
-let tabuleiro= ['      ',
-'      ', 
-'      ',
-'      ',
-'      ',
-'      ',
-'      ',]
+let tabuleiro = ['      ',
+    '      ',
+    '      ',
+    '      ',
+    '      ',
+    '      ',
+    '      ',]
+
+
+const JOGADORES = {"jogador1": "", "jogador2": ""}
 
 const Todotabuleiro = document.getElementById('tabuleiro-divs')
-for(let i =0; i<tabuleiro.length;i++){
-const tabuleiroLinha = document.createElement('section')
-tabuleiroLinha.classList.add('tabuleiro-divs__column')
-for(let j =0; j<6;j++){
-const celulaDiv = document.createElement('div')
-celulaDiv.classList.add('tabuleiro-divs__divs')
-tabuleiroLinha.appendChild(celulaDiv)
-}
-Todotabuleiro.appendChild(tabuleiroLinha)
+
+document.querySelector(".startSection--jogadores").addEventListener("click", (event) => {
+    let option = document.createElement("div");
+    if(event.target.className === "startSection--option"){
+        option = event.target.firstElementChild
+    }
+    else if(event.target.className === "option--red" || event.target.className === "option--black"){
+        option = event.target
+    }
+
+    if(option !== undefined){
+        if(option.className === "option--red"){
+            JOGADORES["jogador1"] = "option--red"
+            JOGADORES["jogador2"] = "option--black"
+
+            let divBlack = document.querySelector(".option--black").closest("div.startSection--option")
+            divBlack.style.border = "1px solid gray"
+            divBlack.style.backgroundColor = "transparent"
+
+            let divRed = document.querySelector(".option--red").closest("div.startSection--option")
+            divRed.style.border = "1px solid rgb(0, 0, 122)"
+            divRed.style.backgroundColor = "lightblue"
+        }
+        else{
+            JOGADORES["jogador1"] = "option--black"
+            JOGADORES["jogador2"] = "option--red"
+
+            let divBlack = document.querySelector(".option--black").closest("div.startSection--option")
+            divBlack.style.border = "1px solid rgb(0, 0, 122)"
+            divBlack.style.backgroundColor = "lightblue"
+
+            let divRed = document.querySelector(".option--red").closest("div.startSection--option")
+            divRed.style.border = "1px solid gray"
+            divRed.style.backgroundColor = "transparent"
+        }
+    }
+})
+
+document.querySelector(".startSection button").addEventListener("click", () => {
+    if(JOGADORES["jogador1"] !== ""){
+        start()
+    }
+})
+
+
+
+
+
+function start(){
+    const sectionStart = document.querySelector(".startSection")
+    sectionStart.style.display = "none";
+
+    for (let i = 0; i < tabuleiro.length; i++) {
+        const tabuleiroLinha = document.createElement('section')
+        tabuleiroLinha.classList.add('tabuleiro-divs__column')
+        for (let j = 0; j < 6; j++) {
+            const celulaDiv = document.createElement('div')
+            celulaDiv.classList.add('tabuleiro-divs__divs')
+            tabuleiroLinha.appendChild(celulaDiv)
+        }
+        Todotabuleiro.appendChild(tabuleiroLinha)
+    }
 }
